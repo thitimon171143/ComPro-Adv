43a7d24f11efaf63376b2ac75ea02fd7a3478392
thitimon171143 <montha.kam.iam@gmail.com>
2020-09-29 11:53:35 +0700

    week9

---
+++ week9/Ex7-BasicLock.py
@@ -0,0 +1,21 @@
+import logging
+import threading
+import time
+import concurrent.futures
+
+class FakeDatabase:
+    def __init__(self):
+        self.value = 0
+        self._lock = threading.Lock()
+    def locked_update(self,name):
+        logging.info("Thread %s: starting update",name)
+        logging.debug("Thread %s about to lock",name)
+        with self._lock:
+            logging.debug("Thread %s has lock",name)
+            local_copy = self.value
+            local_copy += 1
+            time.sleep(0.1)
+            self.value = local_copy
+            logging.debug("Thread %s about to release lock",name)
+        logging.debug("Thread %s after release",name)
+        logging.info("Thread %s: finishing update",name)
